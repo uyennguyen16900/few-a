053e53d2c334c4e150aeefbea0ec80feb862fec7
uyennguyen16900 <uyennguyen16900@gmail.com>
2020-07-31 15:10:04 -0700

    switched to CSS instead of using canvas and added px

--- style.js
+++ style.js
@@ -1,22 +1,27 @@
 let colorInput = document.querySelector('#colorInput');
-let c = document.getElementById("canvas");
-let ctx = c.getContext("2d");
+// let c = document.getElementById("canvas");
+// let ctx = c.getContext("2d");
+let myElement = document.querySelector("#box");
 
 colorInput.addEventListener('input', () => {
   let color = colorInput.value;
   document.getElementById('hex').innerHTML = color;
-  ctx.fillStyle = color;
-  ctx.fillRect(0,0,50,100);
+  // ctx.fillStyle = color;
+  // ctx.fillRect(0,0,50,100);
+  myElement.style.backgroundColor = color;
 });
+
 
 let widthInput = document.querySelector('#widthInput');
 widthInput.addEventListener('input', () => {
-  document.getElementById('width').innerHTML = widthInput.value;
-  ctx.canvas.width = widthInput.value;
+  document.getElementById('width').innerHTML = "<p>" + widthInput.value + "px</p>";
+  // ctx.canvas.width = widthInput.value;
+  myElement.style.width = widthInput.value + "px";
 });
 
 let heightInput = document.querySelector('#heightInput');
 heightInput.addEventListener('input', () => {
-  document.getElementById('height').innerHTML = heightInput.value;
-  ctx.canvas.height = heightInput.value;
+  document.getElementById('height').innerHTML = "<p>" + heightInput.value + "px</p>";
+  // ctx.canvas.height = heightInput.value;
+  myElement.style.height = heightInput.value + "px";
 });
